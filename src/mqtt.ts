f893a01a67f5189b8ecb810a88e60ac4b01b5936
damienzonly <damianomails@gmail.com>
2022-12-05 01:43:09 +0100

    using env as broker host

--- src/mqtt.ts
+++ src/mqtt.ts
@@ -1,7 +1,7 @@
 import { connect } from "mqtt";
 
 export function createMQTTConnection() {
-    const client = connect('broker.emqx.io');
+    const client = connect(process.env.BROKER_URL);
     client
         .on("connect", () => {
             console.log("client connected");
